83cf22e0ae9736bbd004cdc98f73e9fe4cd89e7c
Oliver Kleine <kleine@itm.uni-luebeck.de>
2015-08-25 19:02:50 +0200

    Only URIs in resource directory

--- ssp-core/src/main/java/eu/spitfire/ssp/utils/Converter.java
+++ ssp-core/src/main/java/eu/spitfire/ssp/utils/Converter.java
@@ -11,10 +11,10 @@ import org.apache.jena.util.SplitIRI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Created by olli on 21.08.15.
@@ -32,6 +32,7 @@ public class Converter {
         }
 
         Map<String, String> prefixes = new HashMap<>();
+        Map<String, Resource> blanks = new HashMap<>();
 
         Model model = ModelFactory.createDefaultModel();
         Resource subject = model.createResource(resource);
@@ -47,13 +48,13 @@ public class Converter {
 
             // parameter ?o
             Node o = binding.get(Var.alloc("o"));
-            RDFNode object = createRDFNode(o, model);
-            ensurePrefixExists(prefixes, object);
+            RDFNode object = createObject(o, model, blanks, prefixes);
 
-            model.add(model.createStatement(subject, predicate, object));
+            subject.addProperty(predicate, object);
 
         }
 
+        // set prefixes for better looking serialization
         for(Map.Entry<String, String> entry : prefixes.entrySet()){
             model.setNsPrefix(entry.getValue(), entry.getKey());
         }
@@ -83,53 +84,15 @@ public class Converter {
 
             // parameter ?o
             Node o = binding.get(Var.alloc("o"));
-            RDFNode object;
-            if(o.isURI() && !o.getURI().contains(":-")){
-                object = model.createResource(o.getURI());
-                ensurePrefixExists(prefixes, object);
-            }
-            else if (o.isLiteral() && !o.toString().contains(":-")){
-                RDFDatatype datatype = o.getLiteralDatatype();
-                if(datatype == null){
-                    object = model.createLiteral(o.getLiteralLexicalForm());
-                }
-                else{
-                    object = model.createTypedLiteral(o.getLiteralLexicalForm(), datatype);
-                    ensurePrefixExists(prefixes, object);
-                }
-            }
-            else {
-                String key = o.toString().replace("\"", "");
-                if(blanks.containsKey(key)){
-                    object = blanks.get(key);
-                }
-                else {
-                    object = model.createResource(AnonId.create(key));
-                    blanks.put(key, (Resource) object);
-                }
-            }
+            RDFNode object = createObject(o, model, blanks, prefixes);
 
             // parameter ?s
             Node s = binding.get(Var.alloc("s"));
-            Resource subject;
-            if(s.isURI() && !s.getURI().contains(":-")){
-                subject = model.createResource(s.getURI());
-                ensurePrefixExists(prefixes, subject);
-            }
-            else {
-                String key = s.toString().replace("\"", "");
-                if(blanks.containsKey(key)){
-                    subject = blanks.get(key);
-                }
-                else {
-                    subject = model.createResource(AnonId.create(key));
-                    blanks.put(key, subject);
-                }
-            }
-
+            Resource subject = createSubject(s, model, blanks, prefixes);
             subject.addProperty(predicate, object);
         }
 
+        // set prefixes for better looking serialization
         for(Map.Entry<String, String> entry : prefixes.entrySet()){
             model.setNsPrefix(entry.getValue(), entry.getKey());
         }
@@ -138,6 +101,73 @@ public class Converter {
 
     }
 
+
+    private static RDFNode createObject(Node node, Model model, Map<String, Resource> blanks, Map<String, String> prefixes){
+        RDFNode object;
+
+        // URI resource (the construction of an actual URI is due to the lazy blank node support of LUPOSDATE)
+        try{
+            if(node.isURI() && new URI(node.getURI()).isAbsolute()){
+                object = model.createResource(node.getURI());
+                ensurePrefixExists(prefixes, object);
+                return object;
+            }
+        }
+        catch (Exception e) {
+            LOG.debug("Doch keine URI...");
+        }
+
+        // Literal (contains(":-") is a hack for LUPOSDATE)
+        if (node.isLiteral() && !node.toString().contains(":-")){
+            RDFDatatype datatype = node.getLiteralDatatype();
+            if(datatype == null){
+                object = model.createLiteral(node.getLiteralLexicalForm());
+            }
+            else{
+                object = model.createTypedLiteral(node.getLiteralLexicalForm(), datatype);
+                ensurePrefixExists(prefixes, object);
+            }
+            return object;
+        }
+
+        // Blank node (the replacement is also a hack for LUPOSDATE)
+        String key = node.toString().replace("\"", "");
+        if(blanks.containsKey(key)){
+            return blanks.get(key);
+        }
+
+        object = model.createResource(AnonId.create(key));
+        blanks.put(key, (Resource) object);
+        return object;
+    }
+
+
+    private static Resource createSubject(Node node, Model model, Map<String, Resource> blanks, Map<String, String> prefixes){
+        Resource subject;
+
+        // URI resource (the construction of an actual URI is due to the lazy blank node support of LUPOSDATE)
+        try{
+            if(node.isURI() && new URI(node.getURI()).isAbsolute()){
+                subject = model.createResource(node.getURI());
+                ensurePrefixExists(prefixes, subject);
+                return subject;
+            }
+        }
+        catch (Exception e) {
+            LOG.debug("Doch keine URI...");
+        }
+
+        // Blank node (the replacement is also a hack for LUPOSDATE)
+        String key = node.toString().replace("\"", "");
+        if(blanks.containsKey(key)){
+            subject = blanks.get(key);
+            return subject;
+        }
+
+        subject = model.createResource(AnonId.create(key));
+        blanks.put(key, subject);
+        return subject;
+    }
 
     private static void ensurePrefixExists(Map<String, String> prefixes, RDFNode node){
         if(node.isURIResource() && !((Resource) node).getURI().contains(":-")){
